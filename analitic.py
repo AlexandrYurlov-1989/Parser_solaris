8c5f565a60d3ea2c7a641531a2f8bd1c52b1ecec
Alexandr <119127991+AlexandrYurlov-1989@users.noreply.github.com>
2023-06-11 15:40:01 +0300

    analitic.py
    Добавлен модуль  Sender2Monitoring, SBISScriptsSettings, Solaris
    
    установлены настройки setting в ручную
    
    установлен аргумент в Metric замена недопустимых символов replace_symbol="/"
    изменено окружение на environment='dev'

--- analitic.py
+++ analitic.py
@@ -1,51 +1,49 @@
 import json
 from Sender2Monitoring import PacketMetrics, Metric
+from SBISScriptsSettings import SBISScriptsSettings
 import time
-import solaris
+import Solaris
 
-solaris.input_mark()
+Solaris.input_mark()
 #  функция парсинга файла data.json и возврата всех значение ключа price - это цены за текущий день на Солярис
 def price():
   with open("data.json", "r", encoding="utf8")  as f:
     data = json.load(f)  # теперь это словарь
-  print(len(data))
+#   print(len(data))
   price = []
   f.close()
   for offer in data:
     if offer['price_info']:
       price.append(offer['price_info']['price'])
 
-  print(price)
+#   print(price)
   return price
 
-price()
+value_price = price()
 
-# settings = {dev-kafkaServers: 'mon-test-kafka1'} # kafka кластер для тестирования. Поднят для экспериментов
+settings = {'dev-kafkaServers': 'mon-test-kafka1'} # kafka кластер для тестирования. Поднят для экспериментов 'dev-kafkaServers': 'mon-test-kafka1'}
 
-# # Получение Time Stamp для метрик
-# time_stamp = round(time.time())
+# Получение Time Stamp для метрик
+time_stamp = round(time.time())
+now = time_stamp
 
-# # Класс PacketMetrics(interval) 
-# # interval - интревал сбора в секундах
-# # Создает объект packet класса PacketMetrics, который будет содержать в себе набор объектов Metric
-# packet = PacketMetrics(60)
+packet = PacketMetrics(60)
 
-# solaris_price = price() # вызов функции и присвоение возвращенного значения
+# Создает объект metric класса Metric
+metric = Metric('Solaris',  # имя метрики
+                now,  # timestamp
+                value_price,  # значение
+                tags={  # набор тэгов
+                    'script': __file__.split('/')[-1:][0],  # рекомендуется для понимания что за скрипт шлет метрику
+                    'contur': 'dev-kafkaServers', # обязательный тэг contur
+                    # если отправка идёт в environment prod, то contur должен быть prod
+                    # если отправка идёт в любой другой environment, то contur может быть любым, но не пустым.
+                    'service': 'someService',  # прочие тэги
+                    'subsystem': 'someSubsystem'
+                    }, replace_symbol="/")
+                
+packet.add_metric(metric)
 
-# metric = Metric('SomeMetricName',  # тэг name
-#                 now,  # timestamp
-#                 solaris_price,  # значение
-#                 tags={  # набор тэгов
-#                     'script': __file__.split('/')[-1:][0],  # рекомендуется для понимания что за скрипт шлет метрику
-#                     'contur': 'prod', # обязательный тэг contur
-#                     # если отправка идёт в environment prod, то contur должен быть prod
-#                     # если отправка идёт в любой другой environment, то contur может быть любым, но не пустым.
-#                     'service': 'someService',  # прочие тэги
-#                     'subsystem': 'someSubsystem'
-#                     })
-#                 # zabbix_metric='SomeZabbixMetric',  # item в заббиксе, если пакет готовится в т.ч. для zabbix
-#                 # zabbix_host_name='SomeZabbixServerName')  # хост в заббиксе, если пакет готовится в т.ч. для zabbix
-
+packet.send_packet(settings, debug=True, environment='dev')
 
 
- 
